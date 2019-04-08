75a644aeaffde7cf62978b858ac58198a911bce3
Anditya Arifianto <anditya@telkomuniversity.ac.id>
2019-04-08 11:33:12 +0700

    Update Docs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
+/**
+ * @license
+ * Copyright 2019 AI Lab - Telkom University. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */ 
+ 
+ 
 import * as tf from '@tensorflow/tfjs'
 import coco_classes from './coco_classes'
 
-
+/**
+* A class that wraps TinyYoloV3.
+*/
 export default class TinyYoloV3 {	
 	
-	constructor(nObject = 20, scoreTh = .2,  iouTh = .3) {
+	/**
+	* @param {nObject} number of maximum object to recognize in one detection
+	* @param {scoreTh} score threshold
+	* @param {iouTh}   Intersect Over Union threshold
+	*/
+	constructor(nObject=20, scoreTh=0.2,  iouTh=0.3) {
 		this.nObject = nObject
 		this.scoreTh = scoreTh
 		this.iouTh = iouTh
@@ -14,10 +39,19 @@ export default class TinyYoloV3 {
 		this.nClass = coco_classes.length
 	}
 	
+	/**
+	* Load the weights model from the specified url
+	* @param {url} link path to model.json weights
+	*/
 	async load( url = 'https://raw.githubusercontent.com/adf-telkomuniv/tfjs-tiny-yolov3/master/model/model.json' ) {
 		this.model = await tf.loadLayersModel(url)
 	}
 	
+	/**
+	* Forward Pass the input and returns the output feature volume
+	* @param {input} image, canvas, or video element
+	* @param {flipHorizontal} flip the input image tensor for webcam input
+	*/
 	async predict(input, flipHorizontal=true) {
 		this.imgSize = input.constructor.name === 'HTMLVideoElement' ? [input.videoHeight, input.videoWidth] : [input.height, input.width]
 		
@@ -41,14 +75,18 @@ export default class TinyYoloV3 {
 		return features		
 	}
 	
-	
+		
+	/**
+	* Forward Pass the input and returns the box of detected objects
+	* @param {input} image, canvas, or video element
+	* @param {flipHorizontal} flip the input image tensor for webcam input
+	*/
 	async detectAndBox(input, flipHorizontal=true){
 
 		const features = await this.predict(input, flipHorizontal)
 		
 		let [boxes, boxScores] = tf.tidy(() => {
 			let nFeature = features.length
-			console.log(nFeature)
 			let anchorMask = this.mask[nFeature]
 			let inputShape = features[0].shape.slice(1, 3).map(num => num * 32)
 
@@ -57,7 +95,8 @@ export default class TinyYoloV3 {
 			let fScores = []
 
 			for (let i = 0; i < nFeature; i++) {
-				const [boxes, boxScores] = this.getFeatureBox( features[i], anchors_tf.gather(tf.tensor1d(anchorMask[i], 'int32')), inputShape)
+				const anchorFeature = anchors_tf.gather(tf.tensor1d(anchorMask[i], 'int32'))
+				const [boxes, boxScores] = this.getFeatureBox( features[i], anchorFeature, inputShape)
 
 				fBoxes.push(boxes)
 				fScores.push(boxScores)
@@ -109,12 +148,12 @@ export default class TinyYoloV3 {
 			return { top, left, bottom, right, height, width, score:scores[i], label:this.labels[labelIdx[i]] }
 		})
 	}
-
+	
 	getFeatureBox( feature, featAnchor, inputShape ) {
 		const nAnchors = featAnchor.shape[0]
 		const anchors_tf = tf.reshape(featAnchor, [1, 1, nAnchors, 2])
 
-		const gridShape = feature.shape.slice(1, 3); // height, width
+		const gridShape = feature.shape.slice(1, 3)
 
 		const gridY = tf.tile(tf.reshape(tf.range(0, gridShape[0]), [-1, 1, 1, 1]), [1, gridShape[1], 1, 1])
 		const gridX = tf.tile(tf.reshape(tf.range(0, gridShape[1]), [1, -1, 1, 1]), [gridShape[0], 1, 1, 1])
@@ -138,7 +177,7 @@ export default class TinyYoloV3 {
 		let boxes = tf.concat([
 			...tf.split(boxMins, 2, 3),
 			...tf.split(boxMaxes, 2, 3)
-		], 3);
+		], 3)
 
 		boxes = boxes.reshape([-1, 4])
 		
@@ -149,3 +188,6 @@ export default class TinyYoloV3 {
 	}
 }
 
+
+// @author ANDITYA ARIFIANTO
+// AI LAB - 2019
